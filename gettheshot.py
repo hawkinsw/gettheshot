2be64451422cfd8d618da50a7db09314fd0ced55
Will Hawkins <whh8b@obs.cr>
2021-04-14 20:35:21 -0400

    Add the ability to calculate the number of doses.

--- gettheshot.py
+++ gettheshot.py
@@ -5,6 +5,7 @@ import json
 import csv
 import time
 import sys
+from datetime import datetime
 
 import boto3
 from boto3.exceptions import S3UploadFailedError
@@ -19,10 +20,16 @@ except ImportError:
 from http.client import HTTPConnection
 import logging
 
+# Assume that each vaccination appointment lasts 30 minutes
+minutes_per_appt = 30
+# Assume that there is one dose per appointment
+doses_per_appt = 1
+
 class SearchResult():
   startDate: Optional[str]
   endDate: Optional[str]
   url: Optional[str]
+  doses: int
   displayAddress: str
   name: str
 
@@ -30,16 +37,19 @@ class SearchResult():
                      displayAddress: str,
                      startDate: Optional[str],
                      endDate: Optional[str],
+                     doses: int,
                      url: Optional[str]):
     self.name = name
     self.displayAddress = displayAddress
     self.startDate = startDate
     self.endDate = endDate
+    self.doses = doses
     self.url = url
 
   def __repr__(self) -> str:
     return self.name + ": " +\
-    self.displayAddress + " (" +\
+    self.displayAddress + " has " + \
+    repr(self.doses) + " doses (" +\
     repr(self.startDate) + ", " +\
     repr(self.endDate) + ") @ " +\
     repr(self.url)
@@ -88,10 +98,60 @@ class AvailabilitySearch():
       return []
 
     def api_output_to_search_result(location: dict) -> SearchResult:
+      location_doses = 0
+
+      # This loop calculates the number of doses available at this location.
+      #
+      # Each location has an array of open hours. Those are the hours and
+      # days that the location is open with vaccines available. The fields
+      # for each entry in the array are:
+      # 1. days: The days of the week for which the _localStart_ and
+      # _localEnd_ apply
+      # 2. localStart: The time that the vaccination center opens on the days
+      # in _days_.
+      # 3. localStart: The time that the vaccination center closes on the days
+      # in _days_.
+      #
+      # We assume that the location schedules minutes_per_appt appointments
+      # and that they can give out doses_per_appt doses per appointment.
+      for open_hour_entry in location['openHours']:
+        # each entry in the openHours could represent more than 1 day.
+        open_hour_day_count = len(open_hour_entry['days'])
+
+        # convert the start time to a datetime (for manipulation)
+        open_hour_start_time = datetime.strptime(
+          open_hour_entry['localStart'],
+          "%H:%M:%S")
+        # convert the end time to a datetime (for manipulation)
+        open_hour_end_time = datetime.strptime(
+          open_hour_entry['localEnd'],
+          "%H:%M:%S")
+        # Take the difference between the start and the end time (in seconds)
+        # convert that to minutes (/60) and then convert that to appt chunks
+        # (/minutes_per_appt). That will be the number of appointments
+        # available. Finally, convert that to doses per day (*doses_per_appt).
+        open_hour_doses_per_day = (
+          int((open_hour_end_time - open_hour_start_time).total_seconds()
+          /60
+          /30)
+          *doses_per_appt)
+
+        """
+        print("location:", json.dumps(location))
+        print("days:", json.dumps(open_hour_entry['days']))
+        print("open_hour_doses_per_day: ", open_hour_doses_per_day)
+        print("open_hour_day_count: ", open_hour_day_count)
+        """
+
+        location_doses += (
+          open_hour_doses_per_day *
+          open_hour_day_count)
+
       return SearchResult(location['name'],
         location['displayAddress'],
         None if location['startDate'] == 'null' else location['startDate'],
         None if location['endDate'] == 'null' else location['endDate'],
+        location_doses,
         None if not 'externalURL' in location else location['externalURL'],
       )
 
